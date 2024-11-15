7b4d3bc864436b29f3277ba5a3b9f8fc44bb9ba0
Keerthana-T-2306 <146418780+Keerthana-T-2306@users.noreply.github.com>
2024-11-15 21:35:55 +0530

    Add files via upload

---
+++ client/src/RouteProtectors/AuthProtector.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from 'react';
+
+const AuthProtector =  ({ children }) => {
+
+  useEffect(() => {
+
+    if (!localStorage.getItem('userType')) {
+      window.location.href = '/';
+    }
+  }, [localStorage]);
+
+
+  return children;
+};
+
+export default AuthProtector;
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,285 @@
+import React, { useContext, useEffect, useState } from 'react'
+import '../styles/LandingPage.css'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { GeneralContext } from '../context/GeneralContext';
+
+const LandingPage = () => {
+
+  const [error, setError] = useState('');
+  const [checkBox, setCheckBox] = useState(false);
+
+
+  const [departure, setDeparture] = useState('');
+  const [destination, setDestination] = useState('');
+  const [departureDate, setDepartureDate] = useState();
+  const [returnDate, setReturnDate] = useState();
+
+
+
+  const navigate = useNavigate();
+  useEffect(()=>{
+    
+    if(localStorage.getItem('userType') === 'admin'){
+      navigate('/admin');
+    } else if(localStorage.getItem('userType') === 'flight-operator'){
+      navigate('/flight-admin');
+    }
+  }, );
+
+  const [Flights, setFlights] = useState([]);
+
+  const fetchFlights = async () =>{
+
+    if(checkBox){
+      if(departure !== "" && destination !== "" && departureDate && returnDate){
+        const date = new Date();
+        const date1 = new Date(departureDate);
+        const date2 = new Date(returnDate);
+        if(date1 > date && date2 > date1){
+          setError("");
+          await axios.get('http://localhost:6001/fetch-flights').then(
+              (response)=>{
+                setFlights(response.data);
+                console.log(response.data)
+              }
+           )
+        } else{ setError("Please check the dates"); }
+      } else{ setError("Please fill all the inputs"); }
+    }else{
+      if(departure !== "" && destination !== "" && departureDate){
+        const date = new Date();
+        const date1 = new Date(departureDate);
+        if(date1 >= date){
+          setError("");
+          await axios.get('http://localhost:6001/fetch-flights').then(
+              (response)=>{
+                setFlights(response.data);
+                console.log(response.data)
+              }
+           )
+        } else{ setError("Please check the dates"); }      
+      } else{ setError("Please fill all the inputs"); }
+    }
+    }
+    const {setTicketBookingDate} = useContext(GeneralContext);
+    const userId = localStorage.getItem('userId');
+
+
+    const handleTicketBooking = async (id, origin, destination) =>{
+      if(userId){
+
+          if(origin === departure){
+            setTicketBookingDate(departureDate);
+            navigate(`/book-flight/${id}`);
+          } else if(destination === departure){
+            setTicketBookingDate(returnDate);
+            navigate(`/book-flight/${id}`);
+          }
+      }else{
+        navigate('/auth');
+      }
+    }
+
+
+
+  return (
+    <div className="landingPage">
+        <div className="landingHero">
+
+
+          <div className="landingHero-title">
+            <h1 className="banner-h1">Embark on an Unforgettable Flight Booking Journey!</h1>
+            <p className="banner-p">Ignite your passion for travel and book exceptional flights that take you to awe-inspiring destinations. Experience the thrill of adventure like never before, as we bring the world closer to you with every booking.</p>     
+          </div>
+
+          
+
+          <div className="Flight-search-container input-container mb-4">
+
+                  {/* <h3>Journey details</h3> */}
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value="" onChange={(e)=>setCheckBox(e.target.checked)} />
+                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Return journey</label>
+                  </div>
+                  <div className='Flight-search-container-body'>
+
+                    <div className="form-floating">
+                      <select className="form-select form-select-sm mb-3"  aria-label=".form-select-sm example" value={departure} onChange={(e)=>setDeparture(e.target.value)}>
+                        <option value="" selected disabled>Select</option>
+                        <option value="Chennai">Chennai</option>
+                        <option value="Banglore">Banglore</option>
+                        <option value="Hyderabad">Hyderabad</option>
+                        <option value="Mumbai">Mumbai</option>
+                        <option value="Indore">Indore</option>
+                        <option value="Delhi">Delhi</option>
+                        <option value="Pune">Pune</option>
+                        <option value="Trivendrum">Trivendrum</option>
+                        <option value="Bhopal">Bhopal</option>
+                        <option value="Kolkata">Kolkata</option>
+                        <option value="varanasi">varanasi</option>
+                        <option value="Jaipur">Jaipur</option>
+                      </select>
+                      <label htmlFor="floatingSelect">Departure City</label>
+                    </div>
+                    <div className="form-floating">
+                      <select className="form-select form-select-sm mb-3"  aria-label=".form-select-sm example" value={destination} onChange={(e)=>setDestination(e.target.value)}>
+                        <option value="" selected disabled>Select</option>
+                        <option value="Chennai">Chennai</option>
+                        <option value="Banglore">Banglore</option>
+                        <option value="Hyderabad">Hyderabad</option>
+                        <option value="Mumbai">Mumbai</option>
+                        <option value="Indore">Indore</option>
+                        <option value="Delhi">Delhi</option>
+                        <option value="Pune">Pune</option>
+                        <option value="Trivendrum">Trivendrum</option>
+                        <option value="Bhopal">Bhopal</option>
+                        <option value="Kolkata">Kolkata</option>
+                        <option value="varanasi">varanasi</option>
+                        <option value="Jaipur">Jaipur</option>
+                      </select>
+                      <label htmlFor="floatingSelect">Destination City</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                      <input type="date" className="form-control" id="floatingInputstartDate" value={departureDate} onChange={(e)=>setDepartureDate(e.target.value)}/>
+                      <label htmlFor="floatingInputstartDate">Journey date</label>
+                    </div>
+                    {checkBox ?
+                    
+                      <div className="form-floating mb-3">
+                        <input type="date" className="form-control" id="floatingInputreturnDate" value={returnDate} onChange={(e)=>setReturnDate(e.target.value)}/>
+                        <label htmlFor="floatingInputreturnDate">Return date</label>
+                      </div>
+                    
+                    :
+                    
+                    ""}
+                    <div>
+                      <button className="btn btn-primary" onClick={fetchFlights}>Search</button>
+                    </div>
+
+                  </div>
+                  <p>{error}</p>
+              </div>
+                  
+                {Flights.length > 0 
+                ?
+                <>
+                {
+                  Flights.filter(Flight => Flight.origin === departure && Flight.destination === destination).length > 0 ? 
+                  <>
+                  <div className="availableFlightsContainer">
+                    <h1>Available Flights</h1>
+
+                    <div className="Flights">
+
+                      {checkBox ?
+                      
+                      <>
+                        {Flights.filter(Flight => (Flight.origin === departure && Flight.destination === destination ) || (Flight.origin === destination && Flight.destination === departure)).map((Flight)=>{
+                        return(
+
+                        <div className="Flight" key={Flight._id}>
+                            <div>
+                                <p> <b>{Flight.flightName}</b></p>
+                                <p ><b>Flight Number:</b> {Flight.flightId}</p>
+                            </div>
+                            <div>
+                                <p ><b>Start :</b> {Flight.origin}</p>
+                                <p ><b>Departure Time:</b> {Flight.departureTime}</p>
+                            </div>
+                            <div>
+                                <p ><b>Destination :</b> {Flight.destination}</p>
+                                <p ><b>Arrival Time:</b> {Flight.arrivalTime}</p>
+                            </div>
+                            <div>
+                                <p ><b>Starting Price:</b> {Flight.basePrice}</p>
+                                <p ><b>Available Seats:</b> {Flight.totalSeats}</p>
+                            </div>
+                            <button className="button btn btn-primary" onClick={()=>handleTicketBooking(Flight._id, Flight.origin, Flight.destination)}>Book Now</button>
+                        </div>
+                        )
+                      })}
+                      </>
+                      :
+                      <>
+                      {Flights.filter(Flight => Flight.origin === departure && Flight.destination === destination).map((Flight)=>{
+                        return(
+
+                        <div className="Flight">
+                            <div>
+                                <p> <b>{Flight.flightName}</b></p>
+                                <p ><b>Flight Number:</b> {Flight.flightId}</p>
+                            </div>
+                            <div>
+                                <p ><b>Start :</b> {Flight.origin}</p>
+                                <p ><b>Departure Time:</b> {Flight.departureTime}</p>
+                            </div>
+                            <div>
+                                <p ><b>Destination :</b> {Flight.destination}</p>
+                                <p ><b>Arrival Time:</b> {Flight.arrivalTime}</p>
+                            </div>
+                            <div>
+                                <p ><b>Starting Price:</b> {Flight.basePrice}</p>
+                                <p ><b>Available Seats:</b> {Flight.totalSeats}</p>
+                            </div>
+                            <button className="button btn btn-primary" onClick={()=>handleTicketBooking(Flight._id, Flight.origin, Flight.destination)}>Book Now</button>
+                        </div>
+                        )
+                      })}
+                      </>}
+
+                      
+
+                    </div>
+                  </div>
+                  </>
+                  :
+                  <>
+                   <div className="availableFlightsContainer">
+                    <h1> No Flights</h1>
+                    </div>
+                  </>
+                }
+                </>
+                :
+                <></>
+                }
+         
+                
+                  
+   
+
+
+
+
+
+
+        </div>
+        <section id="about" className="section-about  p-4">
+        <div className="container">
+            <h2 className="section-title">About Us</h2>
+            <p className="section-description">
+                &nbsp; &nbsp;&nbsp; &nbsp; At FlightGo, we believe that travel should be easy, affordable, and accessible to everyone. Founded with the mission to transform the way people book flights, our platform simplifies the entire process from searching for flights to managing bookings. We are dedicated to helping travelers across the globe find the best flights with ease, so they can focus on what truly matters: the experience of travel
+            </p>
+            <h2 className="section-title">Our Vision</h2>
+            <p className="section-description">
+                &nbsp; &nbsp;&nbsp; &nbsp; Our vision is to become the leading global travel platform that empowers individuals and businesses alike to explore new horizons effortlessly. We aim to revolutionize air travel by providing a seamless, user-friendly service that connects people to destinations worldwide, while offering the most competitive prices and a personalized travel experience.
+            </p>
+            <h2 className="section-title">Our Mission</h2>
+            <p className="section-description">
+                &nbsp; &nbsp;&nbsp; &nbsp; Make travel easier: With a few clicks, our customers can search, compare, and book flights to destinations across the globe.
+Ensure transparency: We believe in clear, upfront pricing—what you see is what you get, without any hidden fees or surprises.
+Provide exceptional support: Our dedicated customer service team is available 24/7 to assist with any inquiries or challenges along the way.
+Offer tailored experiences: We use technology to personalize flight recommendations based on your preferences, ensuring that each journey is just right for you.
+            </p>
+
+            <span><h5>2024 FlightGo Connect - &copy; All rights reserved</h5></span>
+
+        </div>
+    </section>
+    </div>
+  )
+}
+
+export default LandingPage
